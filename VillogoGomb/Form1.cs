4ad6e8ca592dd479265a6c96ba8fdcb6988cfa64
sbarna12 <soosbarnabasfulop@gmail.com>
2024-02-29 14:56:50 +0100

    02.29

---
+++ VillogoGomb/Form1.cs
@@ -0,0 +1,56 @@
+namespace VillogoGomb
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+            //for (int sor = 0; sor < 20; sor++)
+            //{
+            //    for (int oszlop = 0; oszlop < 20; oszlop++)
+            //    {
+            //        VillogoGomb b = new VillogoGomb();
+            //        Controls.Add(b);
+            //        b.Left = oszlop*20;
+            //        b.Top = sor * 20;
+            //        b.Width=20;
+            //        b.Height = 20;
+            //    }
+            //}
+            //for (int i = 0; i < 10; i++)
+            //{
+            //    for (int j = 0; j < 10; j++)
+            //    {
+            //        SzamoloGomb b = new SzamoloGomb();
+            //        Controls.Add(b);
+            //        b.Left = i * 50;
+            //        b.Top = j * 50;
+            //        b.Height = 50;
+            //        b.Width = 50;
+            //    }
+            //}
+
+            //Random rnd = new Random();
+            //for (int i = 0; i < 10000; i++)
+            //{
+            //        Button b = new Button();
+            //        Controls.Add(b);
+            //        b.Left = rnd.Next(ClientRectangle.Width);
+            //        b.Top = rnd.Next(ClientRectangle.Height);
+            //        b.Height = rnd.Next(ClientRectangle.Height/2);
+            //        b.Width = rnd.Next(ClientRectangle.Width/2);
+            //        b.BackColor = Color.FromArgb(rnd.Next(255), rnd.Next(255), rnd.Next(255));
+
+
+            //}
+
+
+
+        }
+    }
+}
---
+++ VillogoGomb/SzamoloGomb.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VillogoGomb
+{
+    internal class SzamoloGomb : Button
+    {
+        int szám = 0;
+        public SzamoloGomb()
+        {
+            Height = 20;
+            Width = 20;
+            MouseClick += SzamoloGomb_MouseClick;
+
+
+        }
+
+        private void SzamoloGomb_MouseClick(object? sender, MouseEventArgs e)
+        {
+
+            if (szám == 5) 
+                szám = 0;
+            szám ++;
+            Text = szám.ToString();
+
+        }
+    }
+}
